a6f83b0629c1344319f6dbe149fcc88aa6892f67
nonatojunior <nonatojunior.rnsj@gmail.com>
2021-08-17 22:36:25 -0300

    melhoria de layout + alterado tamanho botao

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def dia_semana(dia)
+    if dia == "Sun"
+      return "Domingo"
+    elsif dia == "Mon"
+      return "Segunda"
+    elsif dia == "Tue"
+      return "Terça"  
+    elsif dia == "Wed"
+      return "Quarta"
+    elsif dia == "Thu"
+      return "Quinta"
+    elsif dia == "Fri"
+      return "Sexta"
+    elsif dia == "Sat"
+      return "Sábado"
+    end
+  end
 end
